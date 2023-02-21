b103c73154ace00bb4608f66a0957b09669ca6b3
Seaan Wong <seanwong24@live.com>
2023-02-21 09:01:23 +0000

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
+import glob from "tiny-glob";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    lib: {
+      entry: await glob("src/plugins/*.plugin.ts"),
+      formats: ["es"],
+    },
+  },
 });
