588598cdb8e9ae4958aea6a55cf779956f09edf4
Sean Wong <seanwong24@live.com>
2023-02-23 06:26:45 +0000

    add geojson layer plugin

---
+++ src/plugins/geojson-layer.plugin.ts
@@ -0,0 +1,42 @@
+import { GeoJsonObject } from "geojson";
+import { LayerType, leaflet } from "gwf-vis-host";
+import { property } from "lit/decorators.js";
+import { GWFVisMapLayerPluginBase } from "../utils/map-layer-base";
+
+export default class GWFVisPluginGeoJSONLayer extends GWFVisMapLayerPluginBase {
+  #geojsonLayerInstance?: leaflet.GeoJSON;
+
+  @property() displayName: string = "geojson layer";
+  @property() type: LayerType = "overlay";
+  @property() active: boolean = false;
+  @property() geojson?: GeoJsonObject | GeoJsonObject[] | string;
+  @property() options?: leaflet.GeoJSONOptions;
+
+  obtainHeader = () => `GeoJSON Layer - ${this.displayName}`;
+
+  protected override initializeMapLayer() {
+    this.#geojsonLayerInstance &&
+      this.removeMapLayerCallback?.(this.#geojsonLayerInstance);
+    this.#geojsonLayerInstance = this.leaflet?.geoJSON(this.obtainGeoJSON(), {
+      ...this.options,
+      pointToLayer: (_feature, latlng) =>
+        new globalThis.L.CircleMarker(latlng, { radius: 10 }),
+    });
+    this.#geojsonLayerInstance &&
+      this.addMapLayerCallback?.(
+        this.#geojsonLayerInstance,
+        this.displayName,
+        this.type,
+        this.active
+      );
+  }
+
+  private obtainGeoJSON() {
+    if (typeof this.geojson === "string") {
+      return JSON.parse(this.geojson);
+    }
+    if (typeof this.geojson === "object") {
+      return this.geojson;
+    }
+  }
+}
