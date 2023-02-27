31930c226c4fb66dca6ab8e1be6cf18176cde199
Sean Wong <seanwong24@live.com>
2023-02-27 21:29:32 +0000

    update geojson layer data fetching logic

--- src/utils/basic.ts
+++ src/utils/basic.ts
@@ -23,3 +23,20 @@ export type GWFVisDefaultPluginWithData = GWFVisPluginWithData<
   string,
   initSqlJs.QueryExecResult | undefined
 >;
+
+export type CallerPlugin = GWFVisDefaultPluginWithData &
+  GWFVisDefaultPluginSharedStates;
+
+export async function runAsyncWithLoading(
+  callback: (() => Promise<void>) | undefined,
+  callerPlugin: CallerPlugin | undefined
+) {
+  const loadingEndDelegate = callerPlugin?.notifyLoadingDelegate?.();
+  await new Promise<void>((resolve) =>
+    setTimeout(async () => {
+      await callback?.();
+      loadingEndDelegate?.();
+      resolve();
+    })
+  );
+}
