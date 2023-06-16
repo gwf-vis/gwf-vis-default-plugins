8b7b406cf24171a179febf4399305c8f58eb5d17
Sean Wong <seanwong24@live.com>
2023-06-16 06:42:25 +0000

    remove prop in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import glob from "tiny-glob";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   build: {
     lib: {
       entry: await glob("src/plugins/*.plugin.ts"),
